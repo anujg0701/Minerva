5421f92395f4098bf3df1387e868707a0d6e1267
anujg0701 <90618117+anujg0701@users.noreply.github.com>
2022-01-02 20:13:14 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello Indian World");
+        System.out.println("Hello Indian World");
+        System.out.println("Hello Indian World");
+        System.out.println("Hello Indian World");
     }
 }
